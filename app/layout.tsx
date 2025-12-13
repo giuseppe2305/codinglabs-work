1b9ed7c85f57af55dd2482fbada8485ac2ae0877
Giuseppe <cresctelli@gmail.com>
2025-12-13 09:02:02 +0100

    Added Inter font.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 interface Props {
   readonly children: React.ReactNode;
@@ -7,7 +10,7 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className="bg-background text-foreground dark">{children}</body>
+      <body className={`${inter.className} bg-background text-foreground dark`}>{children}</body>
     </html>
   );
 }
