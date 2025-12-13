e153891ba30684c9be1fed341a815d917d6ac3e9
Giuseppe <cresctelli@gmail.com>
2025-12-13 09:44:22 +0100

    Layout fixes.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <header className="fixed py-3 w-full items-center">
       <div className="w-[95%] grid grid-cols-[1fr_auto_1fr] items-center mx-auto">
-        <div className="size-12 relative">
+        <div className="size-9 relative">
           <Image fill src="/logo.svg" alt="logo" sizes="48px" />
         </div>
         <nav className="flex items-center gap-12">
--- components/ShadowBlob.tsx
+++ components/ShadowBlob.tsx
@@ -1,6 +1,6 @@
 function ShadowBlob() {
   return (
-    <div className="absolute top-10 left-1/2 -translate-x-1/2 size-200 bg-primary/5 z-1 rounded-full filter blur-[100px] blob"></div>
+    <div className="absolute top-10 left-1/2 -translate-x-1/2 size-200 bg-primary/5 -z-1 rounded-full filter blur-[100px]"></div>
   );
 }
 
