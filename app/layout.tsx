362a7e4e8aca15e61e9923e7974fd42ffa8c3583
Giuseppe <cresctelli@gmail.com>
2025-12-18 08:13:40 +0100

    Added Navbar to global layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import type { ChildrenNode } from "@/types/ChildrenNode";
 
 import { Analytics } from "@vercel/analytics/next";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: ChildrenNode) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-background text-foreground dark flex flex-col max-w-screen`}>
+        <Navbar />
         {children}
         <Analytics />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { Features } from "@/components/Features";
 import { Hero } from "@/components/Hero";
-import { Navbar } from "@/components/Navbar";
 
 export default function page() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Features />
     </>
