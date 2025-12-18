081d1bf187e263deabafa55b99661a8ee6a1f6a8
Giuseppe <cresctelli@gmail.com>
2025-12-18 19:31:44 +0100

    Created docs homepage.

--- app/documentazione/page.tsx
+++ app/documentazione/page.tsx
@@ -1,3 +1,26 @@
+import { LanguageCard } from "@/components/LanguageCard";
+import { Section } from "@/components/Section/Section";
+import { Button } from "@/components/ui/button";
+
 export default function page() {
-  return <div>Documentazione</div>;
+  return (
+    <>
+      <Section className="border-b">
+        <div className="h-100 mt-30 w-1/2 flex flex-col justify-center">
+          <Section.Title>Documentazioni</Section.Title>
+          <Section.Subtitle>
+            Esplora la nostra raccolta completa di guide, tutorial e documentazione tecnica. Dalle basi alla padronanza
+            avanzata, con esempi pratici e approfondimenti dettagliati.
+          </Section.Subtitle>
+          <Button className="self-start mt-10">Suggerisci Modifiche</Button>
+        </div>
+      </Section>
+      <Section className="grid grid-cols-3 gap-10 mt-4">
+        <LanguageCard />
+        <LanguageCard />
+        <LanguageCard />
+        <LanguageCard />
+      </Section>
+    </>
+  );
 }
