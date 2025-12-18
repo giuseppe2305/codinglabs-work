42b96337a8be2fdb8ac5429a7b4512b4a2447055
Giuseppe <cresctelli@gmail.com>
2025-12-18 19:03:40 +0100

    Implemented className to add customization for the Section.

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -2,9 +2,12 @@ import { Title } from "./Title";
 import { Subtitle } from "./Subtitle";
 
 import type { ChildrenNode } from "@/types/ChildrenNode";
+import type { ClassName } from "@/types/ClassName";
 
-function Section({ children }: ChildrenNode) {
-  return <section className="w-[90%] mx-auto py-15">{children}</section>;
+type Props = ChildrenNode & ClassName;
+
+function Section({ children, className }: Props) {
+  return <section className={`w-[90%] mx-auto py-15 ${className}`}>{children}</section>;
 }
 
 Section.Title = Title;
