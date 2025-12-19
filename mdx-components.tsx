47b4b5fdaef1f08e05b542cdade32f8e927f3229
Giuseppe <cresctelli@gmail.com>
2025-12-19 07:42:28 +0100

    Improved text styling.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,7 +2,7 @@ import type { MDXComponents } from "mdx/types";
 
 const components: MDXComponents = {
   h1: ({ children }) => <h1 className="text-accent-foreground text-3xl font-bold pb-3">{children}</h1>,
-  h2: ({ children }) => <h2 className="text-accent-foreground text-2xl font-bold pt-8 pb-3">{children}</h2>,
+  h2: ({ children }) => <h2 className="text-accent-foreground text-xl font-bold pt-8 pb-3">{children}</h2>,
 
   p: ({ children }) => <p>{children}</p>,
 };
