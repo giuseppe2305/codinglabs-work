90d743b443bdc755ae5f449483e3dddf72eb59e6
Giuseppe <cresctelli@gmail.com>
2025-12-19 07:35:21 +0100

    Implemented more stylind to MDX.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,9 @@ import type { MDXComponents } from "mdx/types";
 
 const components: MDXComponents = {
   h1: ({ children }) => <h1 className="text-accent-foreground text-3xl font-bold pb-3">{children}</h1>,
+  h2: ({ children }) => <h2 className="text-accent-foreground text-2xl font-bold pt-8 pb-3">{children}</h2>,
+
+  p: ({ children }) => <p>{children}</p>,
 };
 
 export function useMDXComponents(): MDXComponents {
