110a88dff59bafa0351849a52fc1ae8a3a4353db
Giuseppe <cresctelli@gmail.com>
2025-12-19 22:18:35 +0100

    Built docs navigation layout menu.

--- app/documentazione/javascript/page.tsx
+++ app/documentazione/javascript/page.tsx
@@ -3,8 +3,22 @@ import Content from "./content.mdx";
 
 export default function Page() {
   return (
-    <Section className="grid grid-cols-[220px_auto_220px] mt-20">
-      <aside>BASE JAVASCRIPT</aside>
+    <Section className="grid grid-cols-[250px_auto_220px] gap-10 mt-20">
+      <aside>
+        <div className="flex flex-col text-sm gap-4">
+          <span className="px-3 font-medium uppercase border-b py-1 not-first:mt-4">Introduzione a Javascript</span>
+          <div>Introduzione</div>
+          <div>Versioni</div>
+          <div>Storia</div>
+          <div>Sintassi</div>
+
+          <span className="px-3 font-medium uppercase border-b py-1 not-first:mt-4">Introduzione a Javascript</span>
+          <div>Introduzione</div>
+          <div>Versioni</div>
+          <div>Storia</div>
+          <div>Sintassi</div>
+        </div>
+      </aside>
       <article>
         <Content />
       </article>
