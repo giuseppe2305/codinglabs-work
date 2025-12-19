59be9ff0aa6536ab92a01d920a5112571bb11b12
Giuseppe <cresctelli@gmail.com>
2025-12-19 07:41:27 +0100

    Added Link to documentation.

--- components/LanguageCard.tsx
+++ components/LanguageCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import Link from "next/link";
 
 interface Props {
   title: string;
@@ -7,15 +8,17 @@ interface Props {
 
 function LanguageCard({ title, description }: Props) {
   return (
-    <Card className="w-100 hover:bg-secondary cursor-pointer">
-      <CardContent>
-        <CardTitle className="text-lg flex items-center gap-3">
-          <div className="size-1 rounded-full bg-primary"></div>
-          {title}
-        </CardTitle>
-        <CardDescription className="mt-4">{description}</CardDescription>
-      </CardContent>
-    </Card>
+    <Link href={`/documentazione/${title.toLowerCase()}`}>
+      <Card className="w-100 hover:bg-secondary cursor-pointer">
+        <CardContent>
+          <CardTitle className="text-lg flex items-center gap-3">
+            <div className="size-1 rounded-full bg-primary"></div>
+            {title}
+          </CardTitle>
+          <CardDescription className="mt-4">{description}</CardDescription>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
 
