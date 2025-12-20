b513cb53915e945dc200a1f023cd9cabdd908f66
Giuseppe <cresctelli@gmail.com>
2025-12-20 08:35:47 +0100

    Added mock data to test rendering.

--- app/documentazione/javascript/page.tsx
+++ app/documentazione/javascript/page.tsx
@@ -3,10 +3,22 @@ import Content from "./content.mdx";
 import { AsideNavigation } from "@/components/AsideNavigation";
 import { AsideIndex } from "@/components/AsideIndex";
 
+const navigation = [
+  {
+    subtitle: "Introduzione a Javascript",
+    content: ["Introduzione", "Storia", "versioni"],
+  },
+
+  {
+    subtitle: "Sintassi",
+    content: ["Sintassi di base", "Commenti", "Variabili"],
+  },
+];
+
 export default function Page() {
   return (
     <Section className="grid grid-cols-[250px_auto_220px] gap-10 mt-20">
-      <AsideNavigation />
+      <AsideNavigation navigation={navigation} />
       <article>
         <Content />
       </article>
