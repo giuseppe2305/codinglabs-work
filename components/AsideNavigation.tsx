1c5963a96ae2e38bd65c283ebca8601e08338756
Giuseppe <cresctelli@gmail.com>
2025-12-20 08:34:07 +0100

    Built AsideNavigation structure.

--- components/AsideNavigation.tsx
+++ components/AsideNavigation.tsx
@@ -1,9 +1,28 @@
 import { HoverLink } from "@/components/HoverLink";
 
-function AsideNavigation() {
+interface AsideNavigation {
+  subtitle: string;
+  content: string[];
+}
+
+interface Props {
+  navigation: AsideNavigation[];
+}
+
+function AsideNavigation({ navigation }: Props) {
   return (
-    <aside>
-      <div className="flex flex-col text-sm gap-4">
+    <aside className="flex flex-col">
+      {navigation.map((section) => (
+        <div key={section.subtitle}>
+          <span>{section.subtitle}</span>
+          <div>
+            {section.content.map((page) => (
+              <div key={page}>{page}</div>
+            ))}
+          </div>
+        </div>
+      ))}
+      {/* <div className="flex flex-col text-sm gap-4">
         <span className="px-3 font-medium uppercase border-b py-1 not-first:mt-4">Introduzione a Javascript</span>
         <HoverLink href="#">Introduzione</HoverLink>
         <HoverLink href="#">Versioni</HoverLink>
@@ -15,7 +34,7 @@ function AsideNavigation() {
         <HoverLink href="#">Versioni</HoverLink>
         <HoverLink href="#">Storia</HoverLink>
         <HoverLink href="#">Sintassi</HoverLink>
-      </div>
+      </div> */}
     </aside>
   );
 }
