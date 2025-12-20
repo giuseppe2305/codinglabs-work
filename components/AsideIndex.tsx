fa8654f6a5b017e2ee11c3ebb35747c4c68324c7
Giuseppe <cresctelli@gmail.com>
2025-12-20 08:59:18 +0100

    Styled AsideIndex.

--- components/AsideIndex.tsx
+++ components/AsideIndex.tsx
@@ -1,10 +1,20 @@
+import { AsideSubtitle } from "@/components/AsideSubtitle";
+
 function AsideIndex() {
   return (
-    <aside className="text-sm flex flex-col gap-4">
-      <div className="uppercase font-medium border-b px-3 text-accent-foreground">Indice</div>
-      <div>Introduzione a Javascript</div>
-      <div>Lo sviluppo di Javascript</div>
-      <div>Un linguaggio complesso</div>
+    <aside className="text-sm flex flex-col">
+      <AsideSubtitle>Indice</AsideSubtitle>
+      <div className="flex flex-col gap-2">
+        <div className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer">
+          Introduzione a Javascript
+        </div>
+        <div className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer">
+          Lo sviluppo di Javascript
+        </div>
+        <div className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer">
+          Un linguaggio complesso
+        </div>
+      </div>
     </aside>
   );
 }
