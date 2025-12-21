ee8971bdc3af7ac5059a2e51b11226e3e9acd94f
Giuseppe <cresctelli@gmail.com>
2025-12-21 19:30:45 +0100

    Removed aside index placeholders.

--- components/AsideIndex.tsx
+++ components/AsideIndex.tsx
@@ -4,17 +4,6 @@ function AsideIndex() {
   return (
     <aside className="text-sm flex flex-col">
       <AsideSubtitle>Indice</AsideSubtitle>
-      <div className="flex flex-col gap-2">
-        <div className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer">
-          Introduzione a Javascript
-        </div>
-        <div className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer">
-          Lo sviluppo di Javascript
-        </div>
-        <div className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer">
-          Un linguaggio complesso
-        </div>
-      </div>
     </aside>
   );
 }
