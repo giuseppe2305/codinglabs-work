d11e17858e3e1eca8347cf675969b54619194864
Giuseppe <cresctelli@gmail.com>
2025-12-21 19:16:10 +0100

    Implemented Typescript typing to params promise.

--- app/documentazione/[topic]/[slug]/page.tsx
+++ app/documentazione/[topic]/[slug]/page.tsx
@@ -1,7 +1,11 @@
 import { ContentSource } from "@/components/ContentSource";
 import { getDoc } from "@/lib/getDoc";
 
-export default async function page({ params }) {
+interface Props {
+  params: Promise<{ topic: string; slug: string }>;
+}
+
+export default async function page({ params }: Props) {
   const paramsData = await params;
   const source = getDoc(paramsData.topic, paramsData.slug);
 
