5b6d3099ec3f08a057925ecd5298475262f7fda9
Giuseppe <cresctelli@gmail.com>
2025-12-21 19:51:40 +0100

    Improved layout styling.

--- app/documentazione/[topic]/layout.tsx
+++ app/documentazione/[topic]/layout.tsx
@@ -4,10 +4,16 @@ import { AsideIndex } from "@/components/AsideIndex";
 import { AsideNavigation } from "@/components/AsideNavigation";
 import { Section } from "@/components/Section/Section";
 
-export default function layout({ children }: ChildrenNode) {
+interface Props extends ChildrenNode {
+  params: Promise<{ topic: string }>;
+}
+
+export default async function layout({ children, params }: Props) {
+  const { topic } = await params;
+
   return (
-    <Section className="grid grid-cols-[200px_1fr_200px] mt-20">
-      <AsideNavigation navigation={[]} />
+    <Section className="grid grid-cols-[200px_1fr_200px] gap-6 mt-20">
+      <AsideNavigation topic={topic} />
       {children}
       <AsideIndex />
     </Section>
