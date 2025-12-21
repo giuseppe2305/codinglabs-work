5505a117c8aa9e597e292a3229bbd7091294a743
Giuseppe <cresctelli@gmail.com>
2025-12-21 19:51:12 +0100

    Styled AsideNavigation to import articles from content.

--- components/AsideNavigation.tsx
+++ components/AsideNavigation.tsx
@@ -1,32 +1,19 @@
 import { AsideSubtitle } from "@/components/AsideSubtitle";
-import Link from "next/link";
-
-interface AsideNavigation {
-  subtitle: string;
-  content: string[];
-}
+import { getDocsList } from "@/lib/docs";
 
 interface Props {
-  navigation: AsideNavigation[];
+  topic: string;
 }
 
-function AsideNavigation({ navigation }: Props) {
+function AsideNavigation({ topic }: Props) {
+  const navigation = getDocsList(topic);
+
   return (
     <aside className="flex flex-col text-sm">
-      {navigation.map((section) => (
-        <div className="not-first:mt-6" key={section.subtitle}>
-          <AsideSubtitle>{section.subtitle}</AsideSubtitle>
-          <div className="flex flex-col gap-2">
-            {section.content.map((page) => (
-              <Link
-                href={`javascript/${page.toLowerCase()}`}
-                className="hover:bg-accent hover:text-accent-foreground duration-150 px-4 py-1.5 rounded-md cursor-pointer"
-                key={page}
-              >
-                {page}
-              </Link>
-            ))}
-          </div>
+      <AsideSubtitle>Articles</AsideSubtitle>
+      {navigation.map((article) => (
+        <div className="not-first:mt-6" key={article.slug}>
+          <div className="flex flex-col gap-2">{article.title}</div>
         </div>
       ))}
     </aside>
