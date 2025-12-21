0ca2cb74bb409601bb9e549c6bf510cf5f3ba8be
Giuseppe <cresctelli@gmail.com>
2025-12-21 19:12:46 +0100

    Installed MDXRemote to dynamic import mdx files.

--- app/documentazione/[topic]/[slug]/page.tsx
+++ app/documentazione/[topic]/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+
 export default async function page({ params }) {
   const pageInfo = await params;
 
@@ -6,8 +8,8 @@ export default async function page({ params }) {
   );
 
   return (
-    <div>
-      <MDXContent />
+    <div className="mt-20">
+      <MDXRemote source={MDXContent} />
     </div>
   );
 }
