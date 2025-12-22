ffed857273aff3c2ed4a04ad98e33455fda1626e
Giuseppe <cresctelli@gmail.com>
2025-12-22 21:21:17 +0100

    Added ProfileCard component in MDX files.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,31 +1,23 @@
 import type { MDXComponents } from "mdx/types";
+import { ProfileCard } from "./components/ProfileCard";
 
 const components: MDXComponents = {
   h1: ({ children }) => (
-    <h1 className="text-accent-foreground text-3xl font-bold pb-3">
-      {children}
-    </h1>
+    <>
+      <h1 className="text-accent-foreground text-3xl font-bold pb-3">{children}</h1>
+      <ProfileCard />
+    </>
   ),
 
-  h2: ({ children }) => (
-    <h2 className="text-accent-foreground text-xl font-bold pt-8 pb-3">
-      {children}
-    </h2>
-  ),
+  h2: ({ children }) => <h2 className="text-accent-foreground text-xl font-bold pt-8 pb-3">{children}</h2>,
 
-  h3: ({ children }) => (
-    <h2 className="text-accent-foreground text-xl font-bold pt-4 pb-3">
-      {children}
-    </h2>
-  ),
+  h3: ({ children }) => <h2 className="text-accent-foreground text-xl font-bold pt-4 pb-3">{children}</h2>,
 
   ul: ({ children }) => <ul className="my-2">{children}</ul>,
   li: ({ children }) => <li className="list-disc list-inside">{children}</li>,
 
   p: ({ children }) => <p>{children}</p>,
-  pre: ({ children }) => (
-    <pre className="bg-muted px-4 py-2 my-2 rounded-md">{children}</pre>
-  ),
+  pre: ({ children }) => <pre className="bg-muted px-4 py-2 my-2 rounded-md">{children}</pre>,
   hr: () => <hr className="mt-4" />,
 };
 
