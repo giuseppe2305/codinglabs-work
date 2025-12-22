9c8b17d5860acd11d4930d5910777268f3e2c02d
Giuseppe <cresctelli@gmail.com>
2025-12-22 07:24:53 +0100

    Added addFilePrefix to find a mathing file and add 01- ex. prefix.

--- lib/docs.ts
+++ lib/docs.ts
@@ -1,23 +1,30 @@
-import fs from "fs";
+import { readdirSync, readFileSync } from "fs";
 import path from "path";
 
 const CONTENT_PATH = path.join(process.cwd(), "content");
 
 export function getDoc(topic: string, slug: string) {
-  const fullPath = path.join(CONTENT_PATH, topic, `${slug}.mdx`);
+  const file = addFilePrefix(topic, slug);
+  const fullPath = path.join(CONTENT_PATH, topic, `${file}`);
 
-  return fs.readFileSync(fullPath, "utf-8");
+  return readFileSync(fullPath, "utf-8");
 }
 
 export function getDocsList(topic: string) {
   const dir = path.join(CONTENT_PATH, topic);
 
-  return fs
-    .readdirSync(dir)
+  return readdirSync(dir)
     .filter((f) => f.endsWith(".mdx"))
     .sort()
     .map((file) => ({
       slug: file.replace(/^\d+-/, "").replace(".mdx", ""),
       title: file.replace(/^\d+-/, "").replace(".mdx", ""),
     }));
+}
+
+function addFilePrefix(topic: string, slug: string) {
+  const directory = readdirSync(`content/${topic}`);
+  const file = directory.find((file) => file.includes(`${slug}.mdx`));
+
+  return file;
 }
