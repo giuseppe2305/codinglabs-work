4839bea901fb40573dba4dba824c164ec93cfc94
Giuseppe <cresctelli@gmail.com>
2025-12-22 21:21:34 +0100

    Added an alert at the top of pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { Navbar } from "@/components/Navbar";
 import { Alert, AlertTitle } from "@/components/ui/alert";
 import { Smile } from "lucide-react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,14 @@ export default function RootLayout({ children }: ChildrenNode) {
     <html lang="en">
       <body className={`${inter.className} bg-background text-foreground dark flex flex-col max-w-screen`}>
         <Alert className="bg-primary rounded-none flex items-center justify-center">
-          <Smile />
-          <AlertTitle>Attenzione</AlertTitle>
+          <Smile className="translate-y-0!" />
+          <AlertTitle>
+            Benvenuto! il sito attualmente é in costruzione, ti invitiamo a visitare la pagina{" "}
+            <Link href="/documentazione" className="underline">
+              Documentazione
+            </Link>{" "}
+            se vuoi iniziare ad imparare.
+          </AlertTitle>
         </Alert>
         <Navbar />
         {children}
