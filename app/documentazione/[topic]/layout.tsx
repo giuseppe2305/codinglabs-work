584976f909f737c470a069c3342590a5111bd820
Giuseppe <cresctelli@gmail.com>
2025-12-22 21:21:44 +0100

    Removed AsideIndex from docs page content layout.

--- app/documentazione/[topic]/layout.tsx
+++ app/documentazione/[topic]/layout.tsx
@@ -1,6 +1,5 @@
 import type { ChildrenNode } from "@/types/ChildrenNode";
 
-import { AsideIndex } from "@/components/AsideIndex";
 import { AsideNavigation } from "@/components/AsideNavigation";
 import { Section } from "@/components/Section/Section";
 
@@ -15,7 +14,6 @@ export default async function layout({ children, params }: Props) {
     <Section className="grid grid-cols-[200px_1fr_200px] gap-8 mt-10">
       <AsideNavigation topic={topic} />
       {children}
-      <AsideIndex />
     </Section>
   );
 }
