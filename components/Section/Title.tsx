10dd783ff2581a06f30196061904a386284e8904
Giuseppe <cresctelli@gmail.com>
2025-12-23 19:10:06 +0100

    Fixed Title component size for responsive design.

--- components/Section/Title.tsx
+++ components/Section/Title.tsx
@@ -1,7 +1,7 @@
 import type { ChildrenString } from "@/types/ChildrenString";
 
 function Title({ children }: ChildrenString) {
-  return <h2 className="text-2xl sm:text-3xl text-accent-foreground font-semibold">{children}</h2>;
+  return <h2 className="text-3xl text-accent-foreground font-semibold">{children}</h2>;
 }
 
 export { Title };
