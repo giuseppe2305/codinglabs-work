b324405f6e476d8f2b63d3db28a7e5e9c95ba059
Giuseppe <cresctelli@gmail.com>
2025-12-23 07:17:48 +0100

    Fixed conflict.

--- components/AsideTOC.tsx
+++ components/AsideTOC.tsx
@@ -1,5 +1,4 @@
 import { AsideSubtitle } from "@/components/AsideSubtitle";
-<<<<<<< HEAD
 import type { Heading } from "@/types/Heading";
 
 interface Props {
@@ -7,24 +6,15 @@ interface Props {
 }
 
 function AsideTOC({ headings }: Props) {
-=======
-
-function AsideTOC({ headings }) {
-  console.log(headings);
->>>>>>> 17cdd6d308d1ca3a84e6ac8eb5309c58a3a76f1d
   return (
     <aside className="text-sm flex flex-col gap-2">
       <AsideSubtitle>Tabella dei Contenuti</AsideSubtitle>
       {headings.map((h) => (
-<<<<<<< HEAD
         <a
           className="py-1 px-2 duration-150 hover:bg-accent rounded-md hover:text-white"
           key={h.id}
           href={`#${h.id}`}
         >
-=======
-        <a className="py-1 px-2 duration-150 hover:bg-accent rounded-md hover:text-white" key={h.id} href={`#${h.id}`}>
->>>>>>> 17cdd6d308d1ca3a84e6ac8eb5309c58a3a76f1d
           {h.text}
         </a>
       ))}
