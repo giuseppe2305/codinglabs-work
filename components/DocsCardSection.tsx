b049f46a23fc19ab3e8df9dbb9c06323b94fb6ae
Giuseppe <cresctelli@gmail.com>
2025-12-24 18:06:18 +0100

    Created a component to reuse docsCardSection.

---
+++ components/DocsCardSection.tsx
@@ -0,0 +1,28 @@
+import type { DocsTechnology } from "@/types/DocsTechnology";
+
+import { Section } from "./Section/Section";
+import { LanguageCard } from "./LanguageCard";
+
+interface Props {
+  list: DocsTechnology[];
+}
+
+function DocsCardSection({ list }: Props) {
+  return (
+    <Section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-4 max-md:justify-items-center">
+      <h2 className="text-xl font-medium text-accent-foreground md:col-span-2 lg:col-span-3">
+        Linguaggi
+      </h2>
+      {list.map((lan) => (
+        <LanguageCard
+          key={lan.id}
+          title={lan.title}
+          description={lan.description}
+          disabled={lan.disabled}
+        />
+      ))}
+    </Section>
+  );
+}
+
+export { DocsCardSection };
