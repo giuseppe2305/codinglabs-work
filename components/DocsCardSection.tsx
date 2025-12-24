c3e3d7c589287908ac3645184fc1f5944c240c9e
Giuseppe <cresctelli@gmail.com>
2025-12-24 18:06:52 +0100

    Added label integration in DocsCardSection.

--- components/DocsCardSection.tsx
+++ components/DocsCardSection.tsx
@@ -5,13 +5,14 @@ import { LanguageCard } from "./LanguageCard";
 
 interface Props {
   list: DocsTechnology[];
+  label: string;
 }
 
-function DocsCardSection({ list }: Props) {
+function DocsCardSection({ list, label }: Props) {
   return (
     <Section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-4 max-md:justify-items-center">
       <h2 className="text-xl font-medium text-accent-foreground md:col-span-2 lg:col-span-3">
-        Linguaggi
+        {label}
       </h2>
       {list.map((lan) => (
         <LanguageCard
