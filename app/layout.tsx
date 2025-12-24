0ab7c40f7f67d431279bad0b94ceead1484b9ffc
Giuseppe <cresctelli@gmail.com>
2025-12-24 08:05:42 +0100

    Exported PageAlert in its own component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+
+import type { Metadata } from "next";
 import type { ChildrenNode } from "@/types/ChildrenNode";
 
 import { Analytics } from "@vercel/analytics/next";
 import { Navbar } from "@/components/Navbar";
-import { Alert, AlertTitle } from "@/components/ui/alert";
-import { Smile } from "lucide-react";
-import Link from "next/link";
-import { Metadata } from "next";
+import { PageAlert } from "@/components/PageAlert";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,17 +34,7 @@ export default function RootLayout({ children }: ChildrenNode) {
       <body
         className={`${inter.className} bg-background min-h-screen text-foreground dark flex flex-col max-w-screen`}
       >
-        <Alert className="bg-primary rounded-none flex items-center justify-center">
-          <Smile className="translate-y-0!" />
-          <AlertTitle>
-            Benvenuto! il sito attualmente é in costruzione, ti invitiamo a
-            visitare la pagina{" "}
-            <Link href="/documentazione" className="underline">
-              documentazione
-            </Link>{" "}
-            se vuoi iniziare ad imparare.
-          </AlertTitle>
-        </Alert>
+        <PageAlert />
         <Navbar />
         {children}
         <Analytics />
