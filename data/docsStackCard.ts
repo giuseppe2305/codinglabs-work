e48724ba178fb30cab6702619adbec606f8b3725
Giuseppe <cresctelli@gmail.com>
2025-12-24 18:01:51 +0100

    Created an object to store data about docs cards.

---
+++ data/docsStackCard.ts
@@ -0,0 +1,44 @@
+export const languages = [
+  {
+    id: 0,
+    title: "Javascript",
+    description:
+      "Il linguaggio fondamentale per creare interattività nel web moderno, sia lato client che lato server.",
+    disabled: false,
+  },
+  {
+    id: 1,
+    title: "Typescript",
+    description:
+      "Un set di strumenti potente che aggiunge tipi statici a JavaScript per codice più robusto e scalabile.",
+    disabled: true,
+  },
+  {
+    id: 2,
+    title: "Python",
+    description:
+      "Linguaggio versatile e leggibile, leader nel campo dell'intelligenza artificiale e nell'analisi dati.",
+    disabled: true,
+  },
+  {
+    id: 3,
+    title: "HTML",
+    description:
+      "Lo standard essenziale per definire la struttura e i contenuti di ogni pagina navigabile sul web.",
+    disabled: true,
+  },
+  {
+    id: 4,
+    title: "CSS",
+    description:
+      "Il linguaggio stilistico usato per curare l'estetica, il layout e il design responsivo dei siti moderni.",
+    disabled: true,
+  },
+  {
+    id: 5,
+    title: "C++",
+    description:
+      "Linguaggio ad alte prestazioni, ideale per lo sviluppo di sistemi, motori grafici e software complessi.",
+    disabled: true,
+  },
+];
