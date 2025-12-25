a52b764b7129306c56ebd131ed8d83aa03c6c514
Giuseppe <cresctelli@gmail.com>
2025-12-25 19:33:01 +0100

    Removed side border from PageAlert.

--- components/PageAlert.tsx
+++ components/PageAlert.tsx
@@ -3,7 +3,7 @@ import { Alert, AlertTitle } from "./ui/alert";
 
 function PageAlert() {
   return (
-    <Alert className="text-center rounded-none flex items-center justify-center py-1.5">
+    <Alert className="text-center rounded-none flex items-center justify-center py-1.5 border-x-0">
       <AlertTitle className="line-clamp-none">
         Sito in costruzione, ti invitiamo a visitare la pagina{" "}
         <Link href="/documentazione" className="underline">
