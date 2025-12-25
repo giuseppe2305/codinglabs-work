a6b5b3d2f70199e6dbca52085230ed195efc9487
Giuseppe <cresctelli@gmail.com>
2025-12-25 16:42:20 +0100

    Exported courses info in their own file.

---
+++ data/coursesInfo.ts
@@ -0,0 +1,28 @@
+import { CourseInfo } from "@/types/CourseInfo";
+
+export const coursesInfo: CourseInfo[] = [
+  {
+    title: "Javascript 2026",
+    description:
+      "Corso di Javascript aggiornato alla sua versione piú recente del 2026.",
+    length: 12,
+    link: "/",
+    price: 40,
+    tags: ["Frontend Development"],
+
+    id: 1,
+    disabled: true,
+  },
+  {
+    title: "CSS 2026",
+    description:
+      "Corso di Javascript aggiornato alla sua versione piú recente del 2026.",
+    length: 12,
+    link: "/",
+    price: 40,
+    tags: ["Frontend Development"],
+
+    id: 2,
+    disabled: true,
+  },
+];
