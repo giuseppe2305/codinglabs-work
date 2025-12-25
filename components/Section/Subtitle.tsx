07aee07ec39d23ae4d23125327bee00d8aad6470
Giuseppe <cresctelli@gmail.com>
2025-12-25 11:38:10 +0100

    Customized sections subtitles.

--- components/Section/Subtitle.tsx
+++ components/Section/Subtitle.tsx
@@ -1,7 +1,11 @@
 import type { ChildrenString } from "@/types/ChildrenString";
 
 function Subtitle({ children }: ChildrenString) {
-  return <p className="text-lg mt-2 font-light">{children}</p>;
+  return (
+    <p className="md:text-lg max-md:max-w-120 mx-auto mt-2 font-light">
+      {children}
+    </p>
+  );
 }
 
 export { Subtitle };
