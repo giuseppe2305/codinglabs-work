74ce7a27ec81c5b4dbbdecca7fc601e275dc60bb
Giuseppe <cresctelli@gmail.com>
2025-12-25 11:50:50 +0100

    Added shadows to card in DocsStackCards.

--- components/DocsStackCard.tsx
+++ components/DocsStackCard.tsx
@@ -20,10 +20,10 @@ function DocsStackCard({ title, description, disabled = false }: Props) {
       }
     >
       <Card
-        className={`w-full h-full py-3 max-w-100 duration-150 hover:scale-101 ${
+        className={`w-full h-full py-3 max-w-100 duration-150 hover:scale-101 shadow-md hover:shadow-lg ${
           disabled
-            ? "cursor-not-allowed hover:border-destructive/30"
-            : "cursor-pointer hover:bg-secondary hover:border-primary/30"
+            ? "cursor-not-allowed hover:border-destructive/30 hover:shadow-destructive/10"
+            : "cursor-pointer hover:bg-secondary hover:border-primary/30 hover:shadow-primary/10"
         }`}
       >
         <CardContent className="px-4">
