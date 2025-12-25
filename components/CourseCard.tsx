ce2f373294095414fa214ef218615f5b0fea152f
Giuseppe <cresctelli@gmail.com>
2025-12-25 16:33:41 +0100

    Styled disabled course Card state.

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -8,11 +8,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Clock, Info, ShoppingCart } from "lucide-react";
+import { AlertCircle, Clock, Info, ShoppingCart } from "lucide-react";
 
-import { InFutureLabel } from "@/components/InFutureLabel";
+import { Alert, AlertTitle } from "./ui/alert";
 
-function CourseCard() {
+interface Props {
+  disabled?: boolean;
+}
+
+function CourseCard({ disabled }: Props) {
   return (
     <Card className="pt-0 pb-4 gap-0 overflow-hidden rounded-md w-full max-w-120 mx-auto shadow-xl hover:shadow-primary/10 duration-150 hover:scale-101 cursor-pointer">
       <div className="aspect-2/1 bg-linear-to-b from-sky-400 to-sky-700 w-full flex items-center justify-center text-2xl">
@@ -23,31 +27,41 @@ function CourseCard() {
           <Badge className="text-primary bg-primary/5">
             Frontend Development
           </Badge>
-          <InFutureLabel />
         </div>
       </CardHeader>
-      <CardContent className="mt-1">
+      <CardContent className="mt-1 mb-5">
         <CardTitle className="text-lg">CSS Moderno</CardTitle>
         <CardDescription className="line-clamp-2">
           Corso di CSS moderno pensato per chi vuole portare ad un livello
           superiore le proprie competenze in CSS
         </CardDescription>
-        <div className="flex items-center justify-between mt-5">
-          <Badge variant="secondary">
-            <Clock />8 ore
-          </Badge>
-          <span className="text-xl font-bold">$29.99</span>
-        </div>
+        {!disabled && (
+          <div className="flex items-center justify-between mt-5">
+            <Badge variant="secondary">
+              <Clock />8 ore
+            </Badge>
+            <span className="text-xl font-bold">$29.99</span>
+          </div>
+        )}
       </CardContent>
-      <CardFooter className="flex flex-col items-stretch gap-2 mt-5">
-        <Button>
-          <ShoppingCart />
-          Acquista ora
-        </Button>
-        <Button variant="secondary" className="text-foreground">
-          <Info />
-          Maggiori informazioni
-        </Button>
+      <CardFooter className="flex flex-col items-stretch gap-2 mt-auto">
+        {disabled ? (
+          <Alert className="text-foreground">
+            <AlertCircle />
+            <AlertTitle>Attualmente non disponibile</AlertTitle>
+          </Alert>
+        ) : (
+          <>
+            <Button>
+              <ShoppingCart />
+              Acquista ora
+            </Button>
+            <Button variant="secondary" className="text-foreground">
+              <Info />
+              Maggiori informazioni
+            </Button>
+          </>
+        )}
       </CardFooter>
     </Card>
   );
