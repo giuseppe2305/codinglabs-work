557b8595de712f7f18bf2882d68c7419dbde3b71
Giuseppe <cresctelli@gmail.com>
2025-12-25 11:37:35 +0100

    Customized docs page layout.

--- app/documentazione/page.tsx
+++ app/documentazione/page.tsx
@@ -2,21 +2,29 @@ import { DocsStackSection } from "@/components/DocsStackSection";
 import { Section } from "@/components/Section/Section";
 import { Button } from "@/components/ui/button";
 import { frameworks, languages, metaFrameworks } from "@/data/docsStackCard";
+import { AlertCircle, Edit } from "lucide-react";
 
 export default function page() {
   return (
     <>
-      <Section className="border-b">
-        <div className="h-100 md:mt-30 w-full md:w-2/3 lg:w-1/2 flex flex-col max-md:text-center justify-center">
+      <Section>
+        <div className="w-full md:w-2/3 lg:w-1/2 flex flex-col max-md:text-center md:py-15">
           <Section.Title>Documentazione</Section.Title>
           <Section.Subtitle>
             Esplora la nostra raccolta completa di guide, tutorial e
             documentazione tecnica. Dalle basi alla padronanza avanzata, con
             esempi pratici e approfondimenti dettagliati.
           </Section.Subtitle>
-          <Button className="self-center max-md:mt-12 md:self-start mt-6">
-            Suggerisci Modifiche
-          </Button>
+          <div className="flex max-md:mt-10 max-md:flex-col items-center gap-2 mt-6">
+            <Button>
+              <Edit />
+              Suggerisci Modifiche
+            </Button>
+            <Button variant="outline">
+              <AlertCircle />
+              Segnala un problema
+            </Button>
+          </div>
         </div>
       </Section>
       <DocsStackSection label="Linguaggi" list={languages} />
