bf358ea918b3eb88a0153c2dba4ff7f2cf48a551
Giuseppe <cresctelli@gmail.com>
2025-12-25 19:30:37 +0100

    Added image of TailwindCSS course and created in courseInfo.

--- data/coursesInfo.ts
+++ data/coursesInfo.ts
@@ -2,6 +2,19 @@ import { CourseInfo } from "@/types/CourseInfo";
 
 export const coursesInfo: CourseInfo[] = [
   {
+    title: "Corso su TailwindCSS v4",
+    description:
+      "Questo corso di Tailwind CSS è basato sull'uso moderno e corretto del framework, seguendo le best practice attuali. Imparerai a costruire interfacce rapide, coerenti e manutenibili senza CSS inutili o soluzioni improvvisate. L'obiettivo è usare Tailwind in modo professionale, capendo quando sfruttarlo al massimo e quando evitarne gli abusi.",
+    length: 2.5,
+    link: "/",
+    price: 9.99,
+    tags: ["Frontend Development", "Intermedio"],
+
+    id: 1,
+    disabled: false,
+    image: "tailwindcss.png",
+  },
+  {
     title: "Corso JavaScript 2026",
     description:
       "Corso di JavaScript pensato per imparare JavaScript moderno, aggiornato agli standard attuali e alle best practice utilizzate nel mondo reale.",
@@ -10,8 +23,9 @@ export const coursesInfo: CourseInfo[] = [
     price: 40,
     tags: ["Frontend Development"],
 
-    id: 1,
+    id: 2,
     disabled: true,
+    image: "/",
   },
   {
     title: "Corso HTML & CSS 2026",
@@ -22,7 +36,8 @@ export const coursesInfo: CourseInfo[] = [
     price: 40,
     tags: ["Frontend Development"],
 
-    id: 2,
+    id: 3,
     disabled: true,
+    image: "/",
   },
 ];
