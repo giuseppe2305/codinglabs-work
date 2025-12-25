0b3dae639752253ec4f88842684bf0ae1592ce10
Giuseppe <cresctelli@gmail.com>
2025-12-25 12:13:33 +0100

    Improved AsideNavigation semantic tags, layout and styiling.

--- components/AsideNavigation.tsx
+++ components/AsideNavigation.tsx
@@ -1,6 +1,7 @@
 import { AsideSubtitle } from "@/components/AsideSubtitle";
 import { getDocsList } from "@/lib/docs";
 import { ArticleLink } from "./ArticleLink";
+import { InFutureLabel } from "./InFutureLabel";
 
 interface Props {
   topic: string;
@@ -10,17 +11,19 @@ function AsideNavigation({ topic }: Props) {
   const sections = getDocsList(topic);
 
   return (
-    <aside className="flex flex-col text-sm">
+    <aside className="text-sm">
       <ul className="flex flex-col gap-2">
         {sections.map((s) => (
-          <div key={s.section}>
+          <li key={s.section} className="not-first:mt-6">
             <AsideSubtitle>{s.section}</AsideSubtitle>
-            {s.pages.map((p) => (
-              <ArticleLink topic={topic} article={p} key={p} />
-            ))}
-          </div>
+            <ul className="flex flex-col gap-1.5">
+              {s.pages.map((p) => (
+                <ArticleLink topic={topic} article={p} key={p} />
+              ))}
+            </ul>
+          </li>
         ))}
-        <li className="text-center mt-4 italic py-2 text-muted-foreground rounded-md">In arrivo...</li>
+        <InFutureLabel className="self-center mt-4" />
       </ul>
     </aside>
   );
