2250af6a6aaa4ef80a663a355b99f774749b9f32
Giuseppe <cresctelli@gmail.com>
2025-12-25 13:07:25 +0100

    Fixed logo size.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Logo() {
   return (
-    <Link href="/" className="size-9 relative">
+    <Link href="/" className="size-8 relative">
       <Image fill src="/logo/logo-light.svg" alt="logo" sizes="48px" />
     </Link>
   );
