bca92ffbfe7bbc13c8c2c47fb35e41a2dbd3055a
Giuseppe <cresctelli@gmail.com>
2025-12-25 12:58:39 +0100

    Reduced margin in Aside menus.

--- components/AsideSubtitle.tsx
+++ components/AsideSubtitle.tsx
@@ -2,7 +2,7 @@ import { ChildrenString } from "@/types/ChildrenString";
 
 function AsideSubtitle({ children }: ChildrenString) {
   return (
-    <h4 className="px-3 font-semibold mb-3 border-b uppercase py-1">
+    <h4 className="px-3 font-semibold mb-2 border-b uppercase py-1">
       {children}
     </h4>
   );
