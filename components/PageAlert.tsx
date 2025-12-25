014893d8e3346e5ad791c64c582e06cef9e0d8eb
Giuseppe <cresctelli@gmail.com>
2025-12-25 15:33:16 +0100

    Changed color of page alert.

--- components/PageAlert.tsx
+++ components/PageAlert.tsx
@@ -3,7 +3,7 @@ import { Alert, AlertTitle } from "./ui/alert";
 
 function PageAlert() {
   return (
-    <Alert className="bg-primary text-primary-foreground text-center rounded-none flex items-center justify-center py-1.5">
+    <Alert className="text-center rounded-none flex items-center justify-center py-1.5">
       <AlertTitle className="line-clamp-none">
         Sito in costruzione, ti invitiamo a visitare la pagina{" "}
         <Link href="/documentazione" className="underline">
