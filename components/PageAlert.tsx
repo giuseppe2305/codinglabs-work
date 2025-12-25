e1db2948e85d4e9a8486400d82dcfd8eb567fd10
Giuseppe <cresctelli@gmail.com>
2025-12-25 10:43:51 +0100

    Customized PageAlert and made it visible on small screens.

--- components/PageAlert.tsx
+++ components/PageAlert.tsx
@@ -1,14 +1,11 @@
 import Link from "next/link";
 import { Alert, AlertTitle } from "./ui/alert";
-import { Smile } from "lucide-react";
 
 function PageAlert() {
   return (
-    <Alert className="bg-primary text-primary-foreground rounded-none flex items-center justify-center">
-      <Smile className="translate-y-0!" />
-      <AlertTitle>
-        Benvenuto! il sito attualmente é in costruzione, ti invitiamo a visitare
-        la pagina{" "}
+    <Alert className="bg-primary text-primary-foreground text-center rounded-none flex items-center justify-center py-1.5">
+      <AlertTitle className="line-clamp-none">
+        Sito in costruzione, ti invitiamo a visitare la pagina{" "}
         <Link href="/documentazione" className="underline">
           documentazione
         </Link>{" "}
