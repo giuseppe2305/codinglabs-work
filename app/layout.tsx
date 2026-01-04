ad01e10bed6347de266f1386fd1e82fd0e504a27
Giuseppe <cresctelli@gmail.com>
2026-01-04 15:49:06 +0100

    Added background blobs to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,16 @@ export default function RootLayout({ children }: ChildrenNode) {
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <body
-        className={`${inter.className} bg-background text-foreground dark flex flex-col max-w-screen`}
+        className={`${inter.className} bg-background relative text-foreground dark flex flex-col max-w-screen`}
       >
+        <div
+          className="pointer-events-none fixed top-0 -left-50 size-100
+           rounded-full bg-linear-to-r from-lime-500/30 to-blue-500/30 blur-[160px]"
+        ></div>
+        <div
+          className="pointer-events-none fixed bottom-0 -right-32 h-150 w-100
+           rounded-full bg-linear-to-l from-rose-600/40 to-blue-500/40 blur-[160px]"
+        ></div>
         <PageAlert />
         <Navbar />
         <div className="min-h-screen flex flex-col">{children}</div>
