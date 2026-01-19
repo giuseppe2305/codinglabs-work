4cee471cebaef393f75c4b9dde035a782cae41f4
crstelli <crstelli@gmail.com>
2026-01-19 10:22:25 +0100

    Added config file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import createMDX from "@next/mdx";
+import nextra from "nextra";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({
+const withNextra = nextra({
   // Add markdown plugins here, as desired
 });
 
-export default withMDX(nextConfig);
+export default withNextra(nextConfig);
