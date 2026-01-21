4e5b583e4056e5b7afdc12b6fc988d39b2bb2f6e
crstelli <crstelli@gmail.com>
2026-01-21 11:44:25 +0100

    Rebuilt docs layout using a different nextjs root layout

--- app/documentazione/(with-navbar)/layout.tsx
+++ app/documentazione/(with-navbar)/layout.tsx
@@ -1,25 +1,44 @@
 import "@/app/globals.css";
 import { Inter } from "next/font/google";
 
-import { Layout, Navbar } from "nextra-theme-docs";
-import { getPageMap } from "nextra/page-map";
-
-import "nextra-theme-docs/style.css";
+import type { Metadata } from "next";
 import type { ChildrenNode } from "@/types/ChildrenNode";
+
 import { Analytics } from "@vercel/analytics/next";
+import { Navbar } from "@/components/Navbar";
+import { PageAlert } from "@/components/PageAlert";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const navbar = (
-  <Navbar
-    className="md:px-20! lg:px-40!"
-    logo={<p className="text-accent-foreground font-bold">CodingLabs - Documentazioni</p>}
-  />
-);
+export const metadata: Metadata = {
+  title: "Coding Labs",
+  description: "Diventa uno sviluppatore, studiando in italiano.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo/logo-dark.svg",
+      },
 
-export default async function DocsLayout({ children }: ChildrenNode) {
-  const pageMap = await getPageMap("/documentazione/javascript");
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo/logo-light.svg",
+      },
+    ],
+  },
 
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  verification: {
+    google: "1quHgETRe6wnQdGZME0gv2OaIlkwTofowNqYimM9o4k",
+  },
+};
+
+export default function RootLayout({ children }: ChildrenNode) {
   return (
     <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <body className={`${inter.className} bg-background relative text-foreground dark flex flex-col max-w-screen`}>
@@ -31,15 +50,10 @@ export default async function DocsLayout({ children }: ChildrenNode) {
           className="-z-1 pointer-events-none fixed bottom-0 aspect-2/3 xl:-right-32 w-30 sm:w-40 right-0 blur-3xl md:blur-[80px] xl:w-100
            rounded-full bg-linear-to-l from-rose-600/25 to-blue-500/25 xl:blur-[150px]"
         ></div>
-        <Layout
-          editLink={null}
-          feedback={{ content: null }}
-          pageMap={pageMap}
-          navbar={navbar}
-          // ... Your additional layout options
-        >
-          {children}
-        </Layout>
+        <PageAlert />
+        <Navbar />
+        <div className="min-h-screen flex flex-col">{children}</div>
+        <Footer />
         <Analytics />
       </body>
     </html>
