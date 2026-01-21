45f2e61240e187ec212d991043b2574a5d41d82d
crstelli <crstelli@gmail.com>
2026-01-21 10:46:42 +0100

    Improved styiling of nextra docs

--- app/documentazione/(docs)/javascript/layout.tsx
+++ app/documentazione/(docs)/javascript/layout.tsx
@@ -1,8 +1,11 @@
-import { Layout } from "nextra-theme-docs";
+import { Layout, Navbar } from "nextra-theme-docs";
 import { getPageMap } from "nextra/page-map";
 
+import "@/app/globals.css";
 import "nextra-theme-docs/style.css";
 import type { ChildrenNode } from "@/types/ChildrenNode";
+
+const navbar = <Navbar logo={<p className="text-accent-foreground">CodingLabs - Documentazioni</p>} />;
 
 export default async function DocsLayout({ children }: ChildrenNode) {
   const pageMap = await getPageMap("/documentazione/javascript");
@@ -12,6 +15,7 @@ export default async function DocsLayout({ children }: ChildrenNode) {
       editLink={null}
       feedback={{ content: null }}
       pageMap={pageMap}
+      navbar={navbar}
       // ... Your additional layout options
     >
       {children}
