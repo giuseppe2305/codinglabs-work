44b57806ac71906cd69ea8d4183ce8cfc4c811b6
Giuseppe <cresctelli@gmail.com>
2026-01-22 22:21:47 +0100

    Removed Light mode and set default to dark

--- components/nextra/Layout.tsx
+++ components/nextra/Layout.tsx
@@ -12,14 +12,18 @@ const inter = Inter({ subsets: ["latin"] });
 const navbar = (
   <Navbar
     className="md:px-20! lg:px-40!"
-    logo={<p className="text-accent-foreground font-bold">CodingLabs - Documentazioni</p>}
+    logo={
+      <p className="text-accent-foreground font-bold">
+        CodingLabs - Documentazioni
+      </p>
+    }
   />
 );
 
 const banner = (
   <Banner>
-    Le documentazioni sono in costruzione, puoi utilizzare la navigazione sulla sinistra come roadmap per monitorare i
-    tuoi progressi. Grazie!
+    Le documentazioni sono in costruzione, puoi utilizzare la navigazione sulla
+    sinistra come roadmap per monitorare i tuoi progressi. Grazie!
   </Banner>
 );
 
@@ -30,18 +34,20 @@ interface Props extends ChildrenNode {
 function Layout({ children, pageMap }: Props) {
   return (
     <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
-      <body className={`${inter.className} bg-background relative text-foreground dark flex flex-col max-w-screen`}>
+      <body
+        className={`${inter.className} bg-background relative text-foreground dark flex flex-col max-w-screen`}
+      >
         <BackgroundBlobs />
         <NextraLayout
           toc={{ backToTop: "Torna all'inizio", title: "In questa pagina" }}
           lastUpdated={<LastUpdated locale="it">Aggiornato il</LastUpdated>}
-          themeSwitch={{ dark: "Tema scuro", light: "Tema chiaro", system: "Tema di sistema" }}
           editLink={null}
           feedback={{ content: null }}
           pageMap={pageMap}
           navbar={navbar}
           banner={banner}
-          // ... Your additional layout options
+          darkMode={false}
+          nextThemes={{ defaultTheme: "dark" }}
         >
           {children}
         </NextraLayout>
