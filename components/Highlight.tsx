09da202af302dd88f6ebb47060f6c3ff40ccf9f8
crstelli <crstelli@gmail.com>
2026-01-26 12:24:00 +0100

    Customized highlighted text color

--- components/Highlight.tsx
+++ components/Highlight.tsx
@@ -1,7 +1,7 @@
 import type { ChildrenNode } from "@/types/ChildrenNode";
 
 function Highlight({ children }: ChildrenNode) {
-  return <span className="text-primary">{children}</span>;
+  return <span className="bg-clip-text text-transparent bg-linear-to-r from-teal-500 to-primary">{children}</span>;
 }
 
 export { Highlight };
